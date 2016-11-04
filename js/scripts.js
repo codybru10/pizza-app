1329d3704d3bcf6fb9672b59e516d37c6840008c
Cody Brubaker <cody.brubaker@gmail.com>
2016-11-04 09:36:32 -0700

    calculate price based off off size and toppings with prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
 //business logic
-function BuildPizza(size) {
+function BuildPizza(size,pep,bac,mush,ham) {
+debugger;
   this.size = size;
-  this.toppings = [];
+  this.toppings = [pep,bac,mush,ham];
 };
 
-BuildPizza.prototype.cost = function() {
+BuildPizza.prototype.calculateCost = function() {
 debugger;
 var price = 8
 
-  if (this.size = 1) {
+  if (this.size === 1) {
     price += 0;
-  } else if (this.size = 2) {
+  } else if (this.size === 2) {
     price += 3;
-  } else if ( this.size = 3) {
+  } else if ( this.size === 3) {
     price += 5;
+  }
+
+  if (this.toppings[0] === "pepperoni") {
+    price += 1;
+  }
+  if (this.toppings[1] === "bacon") {
+    price +=1;
+  }
+  if (this.toppings[2] === "mushroom") {
+    price +=1;
+  }
+  if (this.toppings[3] === "ham") {
+    price +=1;
   }
   return price;
 };
+
+
 
 //user interface
 $(document).ready(function(){
@@ -24,10 +40,15 @@ $(document).ready(function(){
     event.preventDefault();
 debugger;
     var pizzaSize = parseInt($("#size").val());
-    var result = new BuildPizza(pizzaSize);
 
-    $("#cost").append(result.cost());
-    //var pizzaToppings = $("input:checkbox[name=topping]:checked").val();
+    var pep = $("input#pep:checkbox[name=topping]:checked").val();
+    var bac = $("input#bac:checkbox[name=topping]:checked").val();
+    var mush = $("input#mush:checkbox[name=topping]:checked").val();
+    var ham = $("input#ham:checkbox[name=topping]:checked").val();
+
+    var result = new BuildPizza(pizzaSize,pep,bac,mush,ham);
+
+    $("#cost").append(result.calculateCost());
 
   });
 });
